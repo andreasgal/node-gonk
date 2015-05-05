4b5ecf8581f6293e8ab4195220e2027c80e5736f
Andreas Gal <andreas.gal@gmail.com>
2015-05-04 19:42:47 -0700

    update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,12 @@ NODE_REPO=~/workspace/node
 
 TOOLCHAIN_PATH=$PWD/out/toolchain
 
+export PATH=$TOOLCHAIN_PATH/bin:$PATH
+export AR=arm-linux-androideabi-ar
+export CC=arm-linux-androideabi-gcc
+export CXX=arm-linux-androideabi-g++
+export LINK=arm-linux-androideabi-g++
+
 function build_toolchain() {
     rm -rf $TOOLCHAIN_PATH
     mkdir -p $TOOLCHAIN_PATH
@@ -25,26 +31,17 @@ function build_node() {
     pushd out/node
     git clone $NODE_REPO $1
     cd $1
-    export PATH=$TOOLCHAIN_PATH/bin:$PATH
-    export AR=arm-linux-androideabi-ar
-    export CC=arm-linux-androideabi-gcc
-    export CXX=arm-linux-androideabi-g++
-    export LINK=arm-linux-androideabi-g++
     ./configure \
+	--without-npm \
 	--without-snapshot \
 	--dest-cpu=arm \
 	--dest-os=android
-    make -j16
+    make -j16 install DESTDIR=../../../prebuilt/$1
     popd
-}
-
-function copy_prebuilt() {
-    cp out/node/$1/out/Release/node prebuilt/node-$1
 }
 
 build_toolchain
 
 for version in $VERSIONS; do
     build_node $version
-    copy_prebuilt $version
 done
